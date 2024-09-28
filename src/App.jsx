94dc7caf5e6b9227eb7e2e9cbf8d6fa95fc095e7
John Mark Fuentes <jmcaluyafuentes@gmail.com>
2024-09-28 14:59:30 +1000

    feat: modify chat feed using the component ChatFeed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import { ChatEngine } from 'react-chat-engine'
+import ChatFeed from './components/ChatFeed'
+
 import './App.css'
 
 const App = () => {
+    const projectID = import.meta.env.VITE_PROJECT_ID;
+    const userName = import.meta.env.VITE_USERNAME;
+    const userSecret = import.meta.env.VITE_USER_SECRET;
+
     return (
         <ChatEngine 
             height="100vh"
-            projectID="902d2c8b-ec1d-43aa-8b7f-d2eb80be5f92"
-            userName="admin"
-            userSecret="admin1234"
+            projectID={projectID}
+            userName={userName}
+            userSecret={userSecret}
+            renderChatFeed={(chatAppProps) => <ChatFeed { ...chatAppProps } />}
         />
     )
 }
