e98ed7aa0348beaecc582efda3697f2f25be5868
John Mark Fuentes <jmcaluyafuentes@gmail.com>
2024-09-29 16:35:29 +1000

    feat: add log-in functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { ChatEngine } from 'react-chat-engine'
 import ChatFeed from './components/ChatFeed'
+import LoginForm from './components/LoginForm'
 
 import './App.css'
 
 const App = () => {
+    if (!localStorage.getItem('username')) return <LoginForm />
+
     const projectID = import.meta.env.VITE_PROJECT_ID;
     const userName = import.meta.env.VITE_USERNAME;
     const userSecret = import.meta.env.VITE_USER_SECRET;
---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const LoginForm = () => {
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const authObject = { 'Project-ID': '902d2c8b-ec1d-43aa-8b7f-d2eb80be5f92', 'User-Name': userName, 'User-Secret': password }
+
+        try {
+            // username | password => chatengine -> give messages
+            await axios.get('https://api.chatengine.io/chats', { headers: authObject });
+            
+            // works out -> logged in
+            localStorage.setItem('username', userName);
+            localStorage.setItem('password', password);
+
+            window.location.reload();
+        } catch (error) {
+            // error -> try with new username
+
+        }
+    }
+
+    return (
+        <div className="wrapper">
+            <div className="form">
+                <h1 className="title">Chat Application</h1>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" value={userName} onChange={(e) => setUserName(e.target.value)} className='input' placeholder='Username' required/>
+                    <input type="text" value={password} onChange={(e) => setPassword(e.target.value)} className='input' placeholder='Password' required/>
+                    <div align='center'>
+                        <button type='submit' className='button'>
+                            <span>Start Chatting</span>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default LoginForm
